66fcda49f493559283d01f5a4fb464eded8198bc
ashuthosh-heda <ashuthosh.heda@gmail.com>
2015-11-07 18:22:48 +0530

    Submit Commit

--- src/sortedArrayPositionsChange.cpp
+++ src/sortedArrayPositionsChange.cpp
@@ -15,5 +15,33 @@ NOTES:
 
 void * sortedArrayPositionsChange(int *Arr, int len)
 {
-	return NULL;
+	if (Arr == NULL || len < 0){
+		return NULL;
+	}
+
+	int i = 0, j = len - 1;
+
+	while (i < len){
+		if (Arr[i] > Arr[i + 1]){
+			break;
+		}
+		i++;
+	}
+
+	if (i == len - 1){
+		return 0;
+	}
+
+	while ((j - 1) > 0){
+		if (Arr[j] < Arr[j - 1]){
+			break;
+		}
+		j--;
+	}
+
+	int temp = Arr[i];
+	Arr[i] = Arr[j];
+	Arr[j] = temp;
+
+	return 0;
 }
